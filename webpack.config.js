c69981fcd2ea6d3e1eb0fc167b088e4042699ea7
TakesxiSximada <takesxi.sximada@gmail.com>
2015-09-13 19:33:01 +0900

    packing mithril

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,33 @@
 // -*- coding: utf-8 -*-
+var path = require('path');
 var webpack = require('webpack');
-var html_webpack_plugin = require('hthml-webpack-plugin');
-
 
 module.exports = {
-    }
+    entry: {
+        vendor: [
+            'mithril',
+        ],
+    },
+    output: {
+        path: path.join(__dirname, 'var'),
+        filename: '[name].bundle.js',
+    },
+    module: {
+        loaders: [
+            // { test: /\.less$/, loader: 'style!css!less' }, // use ! to chain loaders
+            { test: /\.css$/, exclude: /\.useable\.css$/, loader: 'style!css' },
+            { test: /\.useable\.css$/, loader: 'style/useable!css' },
+            { test: /\.(png|jpg)$/, loader: 'url?limit=8192' }, // inline base64 URLs for <=8k images, direct URLs for the rest,
+            { test: /\.html$/, loader: "raw" }
+        ]
+    },
+    resolve: {
+        root: [path.join(__dirname, "bower_components")],
+        extensions: ["", ".js"],
+    },
+    plugins: [
+        new webpack.ResolverPlugin(
+            new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
+        ),
+    ],
+};
