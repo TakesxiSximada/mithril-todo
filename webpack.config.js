7b44f114c175241c8a37b43db439c4e717772db6
TakesxiSximada <takesxi.sximada@gmail.com>
2015-09-30 22:58:29 +0900

    packed the package that installed in bower

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,14 @@
 // -*- coding: utf-8 -*-
 var path = require('path');
 var webpack = require('webpack');
+var BowerWebpackPlugin = require("bower-webpack-plugin");
 
 module.exports = {
     entry: {
-        index: './src/index.js',
-        pages: './src/pages.js',
-        users: './src/users.js',
-        vendor: [
-            'mithril',
-        ],
+        'index': './src/index.js',
+        'users': './src/users.js',
+        'pages': './src/pages.js',
+        'vendor': './entry.js',
     },
     output: {
         path: path.join(__dirname, 'var'),
@@ -18,7 +17,9 @@ module.exports = {
     module: {
         loaders: [
             // { test: /\.less$/, loader: 'style!css!less' }, // use ! to chain loaders
-            { test: /\.css$/, exclude: /\.useable\.css$/, loader: 'style!css' },
+            // { test: /\.css$/, exclude: /\.useable\.css$/, loader: 'style!css'},
+            { test: /\.css$/, loader: 'style!css'},
+            { test: /\.(woff|svg|ttf|eot)([\?]?.*)$/, loader: "file-loader?name=[name].[ext]"},
             { test: /\.useable\.css$/, loader: 'style/useable!css' },
             { test: /\.(png|jpg)$/, loader: 'url?limit=8192' }, // inline base64 URLs for <=8k images, direct URLs for the rest,
             { test: /\.html$/, loader: "raw" }
@@ -29,9 +30,15 @@ module.exports = {
         extensions: ["", ".js", ".css"],
     },
     plugins: [
+        new BowerWebpackPlugin({excludes: /.*\.less/}),
         new webpack.ResolverPlugin(
             new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
         ),
-        new webpack.ProvidePlugin({m: "mithril"}),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            m: 'mithril',
+        }),
     ],
 };
